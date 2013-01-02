a916307ce55b0c6891aaca6a845d01ea8929a055
Anthony Cook <anthonymichaelcook@gmail.com>
2013-01-01 20:35:16 -0800

    Fixing misnamed require.

--- lib/remedy.rb
+++ lib/remedy.rb
@@ -1,5 +1,5 @@
 %w{
-  version ansi characters console resized_console content header footer
+  version ansi characters console console_resized content header footer
   interaction key keyboard partial view viewport
 }.each do |lib|
   require "remedy/#{lib}"

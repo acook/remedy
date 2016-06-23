bb48f3c728961d74dad8c70649c5791cf4abf350
Anthony Cook <github@anthonymcook.com>
2016-06-22 22:24:07 -0500

    Expanded list of control characters.
    - Also used ? syntax for single characters instead of quotes.
    - Added some heading comments, but this object will need to be
    refactored down the line anyway.

--- lib/remedy/characters.rb
+++ lib/remedy/characters.rb
@@ -6,6 +6,8 @@ module Remedy
       all[sequence_to_match]
     end
 
+    # Character Groups
+
     def all
       @all ||= printable.merge(nonprintable)
     end
@@ -14,34 +16,41 @@ module Remedy
       @printable ||= whitespace.merge(
         alphabetical.merge(
           numeric.merge(
-            punctuation)))
-    end
-
-    def whitespace
-      {
-        ' '  => :space,
-        "\t" => :tab,
-        "\r" => :carriage_return,
-        "\n" => :line_feed
-      }
+            punctuation
+          )
+        )
+      )
     end
 
     def alphabetical
       @alphabetics ||= get_alphabetics
     end
 
+    def nonprintable
+      @nonprintable ||= special.merge(directional).merge(escape).merge(control)
+    end
+
+    def whitespace
+      {
+        ?\s => :space,
+        ?\t => :tab,
+        ?\r => :carriage_return,
+        ?\n => :line_feed
+      }
+    end
+
     def numeric
       {
-        '0' => :zero,
-        '1' => :one,
-        '2' => :two,
-        '3' => :three,
-        '4' => :four,
-        '5' => :five,
-        '6' => :six,
-        '7' => :seven,
-        '8' => :eight,
-        '9' => :nine
+        ?0 => :zero,
+        ?1 => :one,
+        ?2 => :two,
+        ?3 => :three,
+        ?4 => :four,
+        ?5 => :five,
+        ?6 => :six,
+        ?7 => :seven,
+        ?8 => :eight,
+        ?9 => :nine
       }
     end
 
@@ -77,13 +86,8 @@ module Remedy
       }
     end
 
-    def nonprintable
-      @nonprintable ||= special.merge(directional).merge(escape).merge(control)
-    end
-
     def special
       {
-        "\177"  => :backspace
       }
     end
 
@@ -105,12 +109,23 @@ module Remedy
     end
 
     def control
-      {
-        "\u0003" => :control_c,
-        "\u0004" => :control_d,
-        "\u0012" => :control_r
+      control_chars = {
+        27.chr => :control_left_square_bracket,
+        28.chr => :control_backslash,
+        29.chr => :control_right_square_bracket,
+        30.chr => :control_caret,
+        31.chr => :control_underscore,
+        127.chr => :delete,
+        177.chr => :backspace
       }
+      (?a..?z).each.with_index do |letter, index|
+        control_chars.merge({index.chr => "control_#{letter}".to_sym})
+      end
+
+      control_chars
     end
+
+    # Glyphs and Alternate Names
 
     def gremlins
       {
@@ -137,10 +152,38 @@ module Remedy
 
     def alternate_names
       {
+        control_a: :start_of_heading,
+        control_b: :start_of_text,
         control_c: :end_of_transmission,
         control_d: :end_of_text,
+        control_e: :enquiry,
+        control_f: :acknowledge,
+        control_g: :bel,
+        control_h: :backspace,
+        control_i: :horizontal_tabulation,
+        control_j: :line_feed,
+        control_k: :vertical_tabulation,
+        control_l: :form_feed,
+        control_m: :carriage_return,
+        control_n: :shift_out,
+        control_o: :shift_in,
+        control_p: :data_link_escape,
+        control_q: :device_control_one,
+        control_r: :device_control_two,
+        control_s: :device_control_three,
+        control_t: :device_control_four,
+        control_u: :negative_acknowledge,
+        control_v: :sychnronous_idle,
+        control_w: :end_of_transmission_block,
+        control_x: :cancel,
+        control_y: :end_of_medium,
+        control_z: :substitute,
 
-        control_r: :device_control_two
+        control_left_square_bracket:  :escape,
+        control_backslash:            :file_separator,
+        control_right_square_bracket: :group_separator,
+        control_caret:                :record_separator,
+        control_underscore:           :unit_separator
       }
     end
 

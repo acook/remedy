c791b9b29ca07185512d8a8addb9d5b66dd45c08
Anthony Cook <github@anthonymcook.com>
2023-10-05 08:07:36 -0500

    Document Screenbuffer class

--- lib/remedy/screenbuffer.rb
+++ lib/remedy/screenbuffer.rb
@@ -5,6 +5,12 @@ module Remedy
   # Even most modern terminals do not allow direct access to the character display array.
   # So we create our own, like a DOM, to do our work on before rendering it to the screen.
   class Screenbuffer
+
+    # Create a new screenbuffer.
+    #
+    # @param size [Remedy::Tuple] the number of rows and columns to allocate
+    # @param fill: [String] a character to pre-fill the buffer with
+    # @param nl: [String] the sequence used to separate lines when converted to a string
     def initialize size, fill: " ", nl: ?\n
       @size = size
       @fill = fill[0]
@@ -13,6 +19,26 @@ module Remedy
     end
     attr_accessor :size, :fill, :nl, :buf
 
+    # Replace the contents of the buffer at a given coordinate (from top left).
+    #
+    # Usage with scalar coordinates and simple string:
+    # ```ruby
+    # row = 1
+    # col = 2
+    # screenbuffer[row, col] = "foo"
+    # ```
+    # Usage with Tuple coordinates and line array:
+    # ```ruby
+    # coords = Tuple 3, 4
+    # lines = ["foo\nbar", "baz"]
+    # screenbuffer[coords] = lines
+    # ```
+    #
+    # @param coords [Remedy::Tuple] the coordinates to begin replacing from
+    # @param row [Numeric] the row to start replacing from, 0 indexed
+    # @param col [Numeric] the column to start replacing from, 0 indexed
+    # @param value [String or Enumerable] the content to place into the screenbuffer
+    #   (always the last argument)
     def []= *params
       value = params.pop
       coords = Tuple params.flatten
@@ -20,6 +46,7 @@ module Remedy
       replace_perline coords, value
     end
 
+    # The raw screenbuffer array itself.
     def buf
       @buf ||= new_buf
     end
@@ -30,6 +57,8 @@ module Remedy
       end
     end
 
+    # Convert screenbuffer to single string.
+    # Concatenates the contents of the buffer with the `nl` attribute.
     def to_s
       buf.join nl
     end

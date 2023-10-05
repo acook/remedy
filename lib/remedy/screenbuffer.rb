5c5f5c4a5842ff1d8899f1a2a427dfcbb401222e
Anthony Cook <github@anthonymcook.com>
2023-10-05 06:51:32 -0500

    Add Screenbuffer class

---
+++ lib/remedy/screenbuffer.rb
@@ -0,0 +1,44 @@
+require 'remedy/tuple'
+
+module Remedy
+  # A screenbuffer is an in-memory representation of the terminal display
+  # Even most modern terminals do not allow direct access to the character display array
+  # So we create our own, like a DOM, to do our work on before rendering it to the screen
+  class Screenbuffer
+    def initialize size, fill: " ", nl: ?\n
+      @size = size
+      @fill = fill[0]
+      @nl = nl
+      @buf = new_buf
+    end
+    attr_accessor :size, :fill, :nl, :buf
+
+    def []= *params
+      value = params.pop
+      coords = params.flatten
+
+      if coords.first.is_a? ::Remedy::Tuple then
+        coords = coords.first
+      end
+
+      row = coords[0]
+      col = coords[1]
+
+      buf[row][col] = value
+    end
+
+    def buf
+      @buf ||= new_buf
+    end
+
+    def new_buf
+      Array.new(size.height) do
+        fill * size.width
+      end
+    end
+
+    def to_s
+      buf.join nl
+    end
+  end
+end
---
+++ spec/screenbuffer_spec.rb
@@ -0,0 +1,25 @@
+require_relative 'spec_helper'
+require 'remedy/screenbuffer'
+
+describe Remedy::Screenbuffer do
+  subject(:sb){ described_class.new size, fill: "." }
+  let(:size){ Tuple 2, 2 }
+
+  describe "#to_s" do
+    it "dumps the screenbuffer as a single string" do
+      expected = "..\n.."
+      actual = sb.to_s
+      expect(actual).to eq expected
+    end
+  end
+
+  describe "#[]=" do
+    it "sets the value at a particular location for a single character" do
+      value = "x"
+      expected = "..\n.#{value}"
+      sb[1,1] = value
+      actual = sb.to_s
+      expect(actual).to eq expected
+    end
+  end
+end

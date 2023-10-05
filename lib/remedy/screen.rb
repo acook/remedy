f6298b85ae73ac31eeb36b38b574ef130df17a3b
Anthony Cook <github@anthonymcook.com>
2023-10-05 09:44:55 -0500

    Create simple Screen object - the would-be replacement for Viewport

---
+++ lib/remedy/screen.rb
@@ -0,0 +1,53 @@
+require "remedy/tuple"
+require "remedy/console"
+require "remedy/ansi"
+require "remedy/screenbuffer"
+
+module Remedy
+  class Screen
+    # Create a new Screen object.
+    #
+    # Only one Screen object should be in use at a time
+    #   because they talk directly to the raw terminal.
+    #   But feel free to have multiple available and swap between them,
+    #   Might be good for having multiple workspaces.
+    #
+    # @param auto_resize [Boolean] can be disabled if you are setting up your own console resize hook
+    # @see #resized
+    # @see Console.set_console_resized_hook!
+    def initialize auto_resize: true
+      @buffer = Screenbuffer.new Console.size, fill: "."
+
+      Console.set_console_resized_hook! do |new_size|
+        resized new_size
+      end if auto_resize
+    end
+    attr_accessor :buffer
+
+    # Draw the buffer to the console using raw output.
+    def draw
+      ANSI.screen.safe_reset!
+      Console.output << buffer.to_ansi
+    end
+
+    # This sets the new screen size and rebuilds the buffer before redrawing it.
+    #
+    # Called automatically unless `auto_resize` was set to `false`,
+    #   or if the console resized hook was changed to something else.
+    #
+    # If setting up your own `Console.set_console_resized_hook!` callback
+    #   then you can use this as a starting point:
+    #
+    # ```ruby
+    # Console.set_console_resized_hook! do |new_size|
+    #   my_screen.resized new_size
+    # end
+    # ```
+    #
+    # @param [Remedy::Tuple] the new size of the terminal
+    def resized new_size
+      buffer.size = new_size
+      draw
+    end
+  end
+end

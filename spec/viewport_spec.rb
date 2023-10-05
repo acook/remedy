b86ed44a9afa7405ea41ad2bb4e530d044598404
Anthony Cook <github@anthonymcook.com>
2023-10-04 19:55:34 -0500

    Capture stdout from readme test

--- spec/viewport_spec.rb
+++ spec/viewport_spec.rb
@@ -3,11 +3,17 @@ require 'remedy/viewport'
 
 describe Remedy::Viewport do
   it 'should be able to execute the example code from the readme' do
+    @stdout = $stdout
     joke = ::Remedy::Partial.new
     joke << "Q: What's the difference between a duck?"
     joke << "A: Purple, because ice cream has no bones!"
 
     screen = ::Remedy::Viewport.new
+    sio = StringIO.new
+    $stdout = sio
     screen.draw joke unless ENV['CI']
+    expect(sio.string).to include("ice cream")
+  ensure
+    $stdout = @stdout
   end
 end

51802cc6c817e3ba32922a8a86d3646a1e546e3f
Anthony Cook <github@anthonymcook.com>
2023-10-10 12:46:33 -0500

    Adds several comparison and math methods to Tuple, plus tests and docs

--- lib/remedy/tuple.rb
+++ lib/remedy/tuple.rb
@@ -64,6 +64,7 @@ module Remedy
     # COMPARISON
 
     def == other_tuple
+      return false unless other_tuple.respond_to? :length
       return false unless bijective? other_tuple
 
       self.dimensions.each.with_index do |d, i|
@@ -73,20 +74,119 @@ module Remedy
       true
     end
 
-    def fits_into? size_to_fit_into
-      other_tuple = Tuple(size_to_fit_into)
+    # Three-way comparison operator AKA the "spaceship operator".
+    #
+    # Used for sorting.
+    # @return [-1,0,1] `-1` if `self > other_tuple`, `1` if `self < other_tuple`, `0` otherwise
+    def <=> other_tuple
+      self.aold(other_tuple).magnitude <=> self.aogd(other_tuple).magnitude
+    end
+
+    # Determines if this Tuple is smaller than the other Tuple in all dimensions.
+    # @param other_tuple [Remedy::Tuple]
+    # @return [Boolean] `true` if this Tuple is the same size or smaller, `false` otherwise.
+    def fits_into? other_tuple
+      return false if self.cardinality > other_tuple.cardinality
       cardinality.times.each do |index|
         return false if self[index] > other_tuple[index]
       end
       true
     end
 
-        # Determines if the two tuples have the same number of dimensions
-    # uses `length` on the other object so it can be used in comparison with more types
+    # Determines if the two tuples have the same number of dimensions
+    #   uses `length` on the other object so it can be used in comparison with more types than just Tuples.
+    # @param other_tuple [Remedy::Tuple]
+    # @return [Boolean]
     def bijective? other_tuple
       cardinality == other_tuple.length
     end
     alias_method :sizesame?, :bijective?
+
+    # Returns a Tuple where the dimensions are the absolute values of the current Tuple.
+    # @return [Remedy::Tuple]
+    def abs
+      self.class.new dimensions.map(&:abs)
+    end
+
+    # Returns the total magnitude of all dimensions.
+    # @return [Numeric]
+    def magnitude
+      dimensions.map(&:abs).sum
+    end
+
+    # The area of difference.
+    #
+    # Indicates the magnitude of the difference between two Tuples.
+    #
+    # @param other_tuple [Remedy::Tuple]
+    # @return [Remedy::Tuple]
+    def aod other_tuple
+      result = [cardinality, other_tuple.cardinality].max.times.with_object(self.class.new) do |index, area|
+        if self[index].nil? then
+          area << other_tuple[index].abs
+        elsif other_tuple[index].nil? then
+          area << self[index].abs
+        else
+          difference = other_tuple[index] - self[index]
+
+          area << difference.abs
+        end
+      end
+
+      self.class.tuplify result
+    end
+
+    # The area of lesser difference.
+    #
+    # Indicates where the `other` Tuple is lesser and by how much.
+    #
+    # @param other_tuple [Remedy::Tuple]
+    # @return [Remedy::Tuple]
+    def aold other_tuple
+      result = [cardinality, other_tuple.cardinality].max.times.with_object(self.class.new) do |index, area|
+        if self[index].nil? then
+          area << 0
+        elsif other_tuple[index].nil? then
+          area << self[index]
+        else
+          difference = self[index] - other_tuple[index]
+
+          if difference < 0 then
+            area << 0
+          else
+            area << difference
+          end
+        end
+      end
+
+      self.class.tuplify result
+    end
+
+    # The area of greater difference.
+    #
+    # Indicates where the `other` Tuple is greater and by how much.
+    #
+    # @param other_tuple [Remedy::Tuple]
+    # @return [Remedy::Tuple]
+    def aogd other_tuple
+      result = [cardinality, other_tuple.cardinality].max.times.with_object(self.class.new) do |index, area|
+        if self[index].nil? then
+          area << other_tuple[index]
+        elsif other_tuple[index].nil? then
+          area << 0
+        else
+          difference = other_tuple[index] - self[index]
+
+          if difference < 0 then
+            area << 0
+          else
+            area << difference
+          end
+        end
+      end
+
+      self.class.tuplify result
+    end
 
     # ACCESSORS
 
@@ -144,14 +244,23 @@ module Remedy
       dimensions[index] = value
     end
 
+    # @return [Integer] The number of dimensions in this Tuple.
     def cardinality
       dimensions.length
     end
     alias_method :length, :cardinality
 
+    # @return [Boolean] `true` if all dimensions are zero, otherwise `false`.
     def zero?
       dimensions.all? do |d|
         d == 0
+      end
+    end
+
+    # @return [Boolean] `true` if any dimension is nonzero, otherwise `false`.
+    def nonzero?
+      dimensions.any? do |d|
+        d != 0
       end
     end
 

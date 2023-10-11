7b29e103e2a449d97ce631b814738042b967be07
Anthony Cook <github@anthonymcook.com>
2023-10-11 12:39:15 -0500

    Remove unused frames in test, stub out columnar test

--- spec/frame_align_spec.rb
+++ spec/frame_align_spec.rb
@@ -12,34 +12,6 @@ describe Remedy::Frame do
     f0
   end
 
-  let(:f1) do
-    f1 = described_class.new name: "f1"
-    f1 << "a"
-    f1.size = Tuple 3, 3
-    f1.fill = ":"
-    f1.valign = :center
-    f1.halign = :center
-    f1
-  end
-  let(:f2) do
-    f2 = described_class.new name: "f2"
-    f2 << "b"
-    f2.size = Tuple 3, 3
-    f2.fill = "*"
-    f2.valign = :center
-    f2.halign = :center
-    f2
-  end
-  let(:f3) do
-    f3 = described_class.new name: "f3"
-    f3 << "c"
-    f3.size = Tuple 3, 3
-    f3.fill = "#"
-    f3.valign = :center
-    f3.halign = :center
-    f3
-  end
-
   before do
     f << "foo"
     f << "bar\nbaz"
@@ -98,4 +70,62 @@ describe Remedy::Frame do
     expect(actual).to eq bottomright
   end
 
+  context "columnar" do
+    before do
+      f.arrangement = :columnar
+    end
+
+    xit "does all the things" do
+      topleft      = "foo   \nbar   \nbaz   \n      \n      \n      "
+      topcenter    = " foo  \n bar  \n baz  \n      \n      \n      "
+      topright     = "   foo\n   bar\n   baz\n      \n      \n      "
+
+      centerleft   = "      \nfoo   \nbar   \nbaz   \n      \n      "
+      centercenter = "      \n foo  \n bar  \n baz  \n      \n      "
+      centerright  = "      \n   foo\n   bar\n   baz\n      \n      "
+
+      bottomleft   = "      \n      \n      \nfoo   \nbar   \nbaz   "
+      bottomcenter = "      \n      \n      \n foo  \n bar  \n baz  "
+      bottomright  = "      \n      \n      \n   foo\n   bar\n   baz"
+
+      actual = f.to_s
+      expect(actual).to eq topleft
+
+      f.halign = :center
+      actual = f.to_s
+      expect(actual).to eq topcenter
+
+      f.halign = :right
+      actual = f.to_s
+      expect(actual).to eq topright
+
+      f.valign = :center
+
+      f.halign = :left
+      actual = f.to_s
+      expect(actual).to eq centerleft
+
+      f.halign = :center
+      actual = f.to_s
+      expect(actual).to eq centercenter
+
+      f.halign = :right
+      actual = f.to_s
+      expect(actual).to eq centerright
+
+      f.valign = :bottom
+
+      f.halign = :left
+      actual = f.to_s
+      expect(actual).to eq bottomleft
+
+      f.halign = :center
+      actual = f.to_s
+      expect(actual).to eq bottomcenter
+
+      f.halign = :right
+      actual = f.to_s
+      expect(actual).to eq bottomright
+    end
+  end
 end

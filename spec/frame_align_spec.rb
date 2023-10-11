580f4b0613b281927b533e40f3b9ff5ba1552f0a
Anthony Cook <github@anthonymcook.com>
2023-10-11 13:07:32 -0500

    Comprehensive alignment tests

--- spec/frame_align_spec.rb
+++ spec/frame_align_spec.rb
@@ -7,8 +7,6 @@ describe Remedy::Frame do
   subject(:f) do
     f0 = described_class.new name: "subject"
     f0.available_size = console_size
-    f0.arrangement = :stacked
-    f0.size = :fill
     f0
   end
 
@@ -17,115 +15,255 @@ describe Remedy::Frame do
     f << "bar\nbaz"
   end
 
-  it "does all the things" do
-    topleft      = "foo   \nbar   \nbaz   \n      \n      \n      "
-    topcenter    = " foo  \n bar  \n baz  \n      \n      \n      "
-    topright     = "   foo\n   bar\n   baz\n      \n      \n      "
+  context "stacked arrangement" do
+    before do
+      f.arrangement = :stacked
+    end
 
-    centerleft   = "      \nfoo   \nbar   \nbaz   \n      \n      "
-    centercenter = "      \n foo  \n bar  \n baz  \n      \n      "
-    centerright  = "      \n   foo\n   bar\n   baz\n      \n      "
+    context "fill size" do
+      before do
+        f.size = :fill
+      end
 
-    bottomleft   = "      \n      \n      \nfoo   \nbar   \nbaz   "
-    bottomcenter = "      \n      \n      \n foo  \n bar  \n baz  "
-    bottomright  = "      \n      \n      \n   foo\n   bar\n   baz"
+      it "does all the things" do
+        topleft      = "foo   \nbar   \nbaz   \n      \n      \n      "
+        topcenter    = " foo  \n bar  \n baz  \n      \n      \n      "
+        topright     = "   foo\n   bar\n   baz\n      \n      \n      "
 
-    actual = f.to_s
-    expect(actual).to eq topleft
+        centerleft   = "      \nfoo   \nbar   \nbaz   \n      \n      "
+        centercenter = "      \n foo  \n bar  \n baz  \n      \n      "
+        centerright  = "      \n   foo\n   bar\n   baz\n      \n      "
 
-    f.halign = :center
-    actual = f.to_s
-    expect(actual).to eq topcenter
+        bottomleft   = "      \n      \n      \nfoo   \nbar   \nbaz   "
+        bottomcenter = "      \n      \n      \n foo  \n bar  \n baz  "
+        bottomright  = "      \n      \n      \n   foo\n   bar\n   baz"
 
-    f.halign = :right
-    actual = f.to_s
-    expect(actual).to eq topright
+        actual = f.to_s
+        expect(actual).to eq topleft
 
-    f.valign = :center
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq topcenter
 
-    f.halign = :left
-    actual = f.to_s
-    expect(actual).to eq centerleft
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq topright
 
-    f.halign = :center
-    actual = f.to_s
-    expect(actual).to eq centercenter
+        f.valign = :center
 
-    f.halign = :right
-    actual = f.to_s
-    expect(actual).to eq centerright
+        f.halign = :left
+        actual = f.to_s
+        expect(actual).to eq centerleft
 
-    f.valign = :bottom
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq centercenter
 
-    f.halign = :left
-    actual = f.to_s
-    expect(actual).to eq bottomleft
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq centerright
 
-    f.halign = :center
-    actual = f.to_s
-    expect(actual).to eq bottomcenter
+        f.valign = :bottom
 
-    f.halign = :right
-    actual = f.to_s
-    expect(actual).to eq bottomright
+        f.halign = :left
+        actual = f.to_s
+        expect(actual).to eq bottomleft
+
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq bottomcenter
+
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq bottomright
+      end
+    end
+
+    context "fixed size" do
+      before do
+        f.size = Tuple 5, 5
+      end
+
+      it "does all the things" do
+        topleft      = "foo  \nbar  \nbaz  \n     \n     "
+        topcenter    = " foo \n bar \n baz \n     \n     "
+        topright     = "  foo\n  bar\n  baz\n     \n     "
+
+        centerleft   = "     \nfoo  \nbar  \nbaz  \n     "
+        centercenter = "     \n foo \n bar \n baz \n     "
+        centerright  = "     \n  foo\n  bar\n  baz\n     "
+
+        bottomleft   = "     \n     \nfoo  \nbar  \nbaz  "
+        bottomcenter = "     \n     \n foo \n bar \n baz "
+        bottomright  = "     \n     \n  foo\n  bar\n  baz"
+
+        actual = f.to_s
+        expect(actual).to eq topleft
+
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq topcenter
+
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq topright
+
+        f.valign = :center
+
+        f.halign = :left
+        actual = f.to_s
+        expect(actual).to eq centerleft
+
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq centercenter
+
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq centerright
+
+        f.valign = :bottom
+
+        f.halign = :left
+        actual = f.to_s
+        expect(actual).to eq bottomleft
+
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq bottomcenter
+
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq bottomright
+      end
+    end
+
   end
 
-  context "columnar" do
+  context "columnar arrangement" do
+    let(:console_size) { sizeclass.new 4, 8 }
     before do
       f.arrangement = :columnar
     end
 
-    xit "does all the things" do
-      topleft      = "foo   \nbar   \nbaz   \n      \n      \n      "
-      topcenter    = " foo  \n bar  \n baz  \n      \n      \n      "
-      topright     = "   foo\n   bar\n   baz\n      \n      \n      "
+    context "fill size" do
+      before do
+        f.size = :fill
+      end
 
-      centerleft   = "      \nfoo   \nbar   \nbaz   \n      \n      "
-      centercenter = "      \n foo  \n bar  \n baz  \n      \n      "
-      centerright  = "      \n   foo\n   bar\n   baz\n      \n      "
+      it "does all the things" do
+        topleft      = "foobar  \n   baz  \n        \n        "
+        topcenter    = " foobar \n    baz \n        \n        "
+        topright     = "  foobar\n     baz\n        \n        "
 
-      bottomleft   = "      \n      \n      \nfoo   \nbar   \nbaz   "
-      bottomcenter = "      \n      \n      \n foo  \n bar  \n baz  "
-      bottomright  = "      \n      \n      \n   foo\n   bar\n   baz"
+        centerleft   = "        \nfoobar  \n   baz  \n        "
+        centercenter = "        \n foobar \n    baz \n        "
+        centerright  = "        \n  foobar\n     baz\n        "
 
-      actual = f.to_s
-      expect(actual).to eq topleft
+        bottomleft   = "        \n        \nfoobar  \n   baz  "
+        bottomcenter = "        \n        \n foobar \n    baz "
+        bottomright  = "        \n        \n  foobar\n     baz"
 
-      f.halign = :center
-      actual = f.to_s
-      expect(actual).to eq topcenter
+        actual = f.to_s
+        expect(actual).to eq topleft
 
-      f.halign = :right
-      actual = f.to_s
-      expect(actual).to eq topright
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq topcenter
 
-      f.valign = :center
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq topright
 
-      f.halign = :left
-      actual = f.to_s
-      expect(actual).to eq centerleft
+        f.valign = :center
 
-      f.halign = :center
-      actual = f.to_s
-      expect(actual).to eq centercenter
+        f.halign = :left
+        actual = f.to_s
+        expect(actual).to eq centerleft
 
-      f.halign = :right
-      actual = f.to_s
-      expect(actual).to eq centerright
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq centercenter
 
-      f.valign = :bottom
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq centerright
 
-      f.halign = :left
-      actual = f.to_s
-      expect(actual).to eq bottomleft
+        f.valign = :bottom
 
-      f.halign = :center
-      actual = f.to_s
-      expect(actual).to eq bottomcenter
+        f.halign = :left
+        actual = f.to_s
+        expect(actual).to eq bottomleft
 
-      f.halign = :right
-      actual = f.to_s
-      expect(actual).to eq bottomright
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq bottomcenter
+
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq bottomright
+      end
     end
+
+    context "fixed size" do
+      before do
+        f.size = Tuple 5, 7
+      end
+
+      it "does all the things" do
+        topleft      = "foobar \n   baz \n       \n       \n       "
+        topcenter    = "foobar \n   baz \n       \n       \n       "
+        topright     = " foobar\n    baz\n       \n       \n       "
+
+        centerleft   = "       \nfoobar \n   baz \n       \n       "
+        centercenter = "       \nfoobar \n   baz \n       \n       "
+        centerright  = "       \n foobar\n    baz\n       \n       "
+
+        bottomleft   = "       \n       \n       \nfoobar \n   baz "
+        bottomcenter = "       \n       \n       \nfoobar \n   baz "
+        bottomright  = "       \n       \n       \n foobar\n    baz"
+
+        actual = f.to_s
+        expect(actual).to eq topleft
+
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq topcenter
+
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq topright
+
+        f.valign = :center
+
+        f.halign = :left
+        actual = f.to_s
+        expect(actual).to eq centerleft
+
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq centercenter
+
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq centerright
+
+        f.valign = :bottom
+
+        f.halign = :left
+        actual = f.to_s
+        expect(actual).to eq bottomleft
+
+        f.halign = :center
+        actual = f.to_s
+        expect(actual).to eq bottomcenter
+
+        f.halign = :right
+        actual = f.to_s
+        expect(actual).to eq bottomright
+      end
+    end
+
   end
+
 end
